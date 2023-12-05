732976dd77e24658246a94049db8fbd959579a38
Andrey <andrey.sandbox.edu@gmail.com>
2023-12-05 12:23:27 +0700

    Парвки от 5.12

--- src/components/utils/heroes.jsx
+++ src/components/utils/heroes.jsx
@@ -32,66 +32,6 @@ import gulnaraStartsevaPhoto3Small from "../../images/photos/gulnara_startseva_3
 const heroes = [
   {
     id: 1,
-    name: "Виталий Лезин",
-    title: (
-      <>
-        Все
-        <br />
-        на&nbsp;матч
-      </>
-    ),
-    job: <>ведущий специалист<br/>по&nbsp;спортивной деятельности</>,
-    portrait: vitaliiLezinPort,
-    video: "3MHvqJFWr3k?si=KyK2RkeWAxO6Fmaj",
-    interviewText: (
-      <>
-        <p>
-          Для Виталия спорт&nbsp;&mdash; это&nbsp;и&nbsp;хобби, и&nbsp;работа.
-          Его&nbsp;волонтерство началось с&nbsp;разовых соревнований
-          и&nbsp;мастер-классов для&nbsp;детей из&nbsp;отдаленных ямальских поселков,
-          позже переросло в&nbsp;интересный массовый проект
-          &laquo;Все&nbsp;на&nbsp;матч&raquo;. Инициатива стала победителем конкурса
-          волонтерских проектов{" "}
-          <a href="https://rodnyegoroda.ru/">
-            <span className="refQoutes">&laquo;</span>
-            <span className="refText">Родные города</span>
-            <span className="refQoutes">&raquo;</span>
-          </a>{" "}
-          компании &laquo;Газпром нефть&raquo;.
-        </p>
-        <p>
-          Зачем Виталий занимается с&nbsp;детьми, как&nbsp;совмещает работу
-          на&nbsp;Новопортовском промысле и&nbsp;тренировки, а&nbsp;также
-          чья&nbsp;поддержка оказалась для&nbsp;него особенно важной, узнаете
-          из&nbsp;интервью.
-        </p>
-      </>
-    ),
-    photos: [
-      {
-        id: 0,
-        big: vitaliiLezinPhoto1Big,
-        small: vitaliiLezinPhoto1Small,
-      },
-      {
-        id: 1,
-        big: vitaliiLezinPhoto2Big,
-        small: vitaliiLezinPhoto2Small,
-      },
-      {
-        id: 2,
-        big: vitaliiLezinPhoto3Big,
-        small: vitaliiLezinPhoto3Small,
-      },
-      {
-        id: 3,
-        big: vitaliiLezinPhoto4Big,
-        small: vitaliiLezinPhoto4Small,
-      },
-    ],
-  },
-  {
-    id: 2,
     name: "Гульнара Старцева",
     title: (
       <>
@@ -103,29 +43,33 @@ const heroes = [
     ),
     job: (
       <>
-        руководитель направления<br/>
-        отдела управления договорами<br/>
-        по&nbsp;капитальному строительству<br/>
+        руководитель направления
+        <br />
+        отдела управления договорами
+        <br />
+        по&nbsp;капитальному строительству
+        <br />
         и&nbsp;бурению скважин
       </>
     ),
     portrait: gulnaraStartsevaPort,
-    video: "_C1DvFWpNiE?si=PQcF2ue3fmfxllGX",
+    video: "uBZ1VLiRfEY?si=bYl4JM4M-kjTZjpk",
     interviewText: (
       <>
         <p>
           У&nbsp;Гульнары большой опыт волонтерства. Ей&nbsp;интересно
           подключаться к&nbsp;самым разным благотворительным проектам, которые
           проводят ее&nbsp;коллеги из&nbsp;&laquo;Газпромнефть-Ямала&raquo;.
-          Она&nbsp;уверена: общее дело, будь&nbsp;то экологическая акция на&nbsp;Крайнем
-          Севере или&nbsp;помощь детскому дому в&nbsp;Тюмени, объединяет
-          и&nbsp;сплачивает.
+          Она&nbsp;уверена: общее дело, будь&nbsp;то&nbsp;экологическая акция
+          на&nbsp;Крайнем Севере или&nbsp;помощь детскому дому в&nbsp;Тюмени,
+          объединяет и&nbsp;сплачивает.
         </p>
         <p>
-          Недавно Гульнара инициировала свой волонтерский проект&nbsp;&mdash;
-          решила обучать финансовой грамотности ямальских школьников. Почему
-          ей&nbsp;кажется важным развивать это&nbsp;направление и&nbsp;почему
-          не&nbsp;сможет ограничиться только&nbsp;им, она&nbsp;рассказала NEFT.
+          Недавно Гульнара с&nbsp;коллегами запустила свой волонтерский
+          проект&nbsp;&mdash; про&nbsp;обучение финансовой грамотности ямальских
+          школьников. Почему кажется важным развивать это&nbsp;направление
+          и&nbsp;почему не&nbsp;сможет ограничиться только&nbsp;им,
+          она&nbsp;рассказала NEFT.
         </p>
       </>
     ),
@@ -148,28 +92,35 @@ const heroes = [
     ],
   },
   {
-    id: 3,
+    id: 2,
     name: "Елена Есипенко",
     title: <>Открыла новое в&nbsp;людях и&nbsp;в&nbsp;себе</>,
-    job: <>руководитель направления<br/>в&nbsp;управлении исполнением договоров</>,
+    job: (
+      <>
+        руководитель направления
+        <br />
+        в&nbsp;управлении исполнением договоров
+      </>
+    ),
     portrait: elenaEsipenkoPort,
     video: "iaVxHVPXMFw?si=7QPHcIz4ip-7ftOs",
     interviewText: (
       <>
         <p>
           Елена много лет участвовала в&nbsp;волонтерских проектах в&nbsp;Томске
-          и&nbsp;Омске. Рассказывает, что&nbsp;получает удовольствие, когда помогает
-          <nobr>кому-то</nobr>. Переехав в&nbsp;Тюмень, устроилась работать
+          и&nbsp;Омске. Рассказывает, что&nbsp;получает удовольствие, когда
+          помогает кому-то. Переехав в&nbsp;Тюмень, устроилась работать
           в&nbsp;&laquo;Газпромнефть-Ямал&raquo; и&nbsp;получила поддержку
           коллег. Елена стала волонтером в&nbsp;детских домах, домах
-          престарелых, а&nbsp;в&nbsp;этом году еще подключилась
-          к&nbsp;всероссийскому проекту
+          престарелых, а&nbsp;в&nbsp;этом году еще&nbsp;подключилась
+          к&nbsp;всероссийскому движению
           &laquo;28&nbsp;петель&raquo;&nbsp;&mdash; для&nbsp;помощи недоношенным
-          младенцам и&nbsp;их&nbsp;мамам.
+          малышам и&nbsp;их&nbsp;мамам.
         </p>
         <p>
-          Мы&nbsp;узнали, почему ей&nbsp;близко добровольчество
-          и&nbsp;как&nbsp;оно&nbsp;влияет на&nbsp;ее&nbsp;профессиональную деятельность.
+          Мы&nbsp;узнали, почему Елене близко добровольчество
+          и&nbsp;как&nbsp;оно&nbsp;влияет на&nbsp;ее&nbsp;профессиональную
+          деятельность.
         </p>
       </>
     ),
@@ -200,7 +151,66 @@ const heroes = [
         small: elenaEsipenkoPhoto5Small,
       },
     ],
-  }
+  },
+  {
+    id: 3,
+    name: "Виталий Лезин",
+    title: (
+      <>
+        Все
+        <br />
+        на&nbsp;матч
+      </>
+    ),
+    job: (
+      <>
+        ведущий специалист
+        <br />
+        по&nbsp;спортивной деятельности
+      </>
+    ),
+    portrait: vitaliiLezinPort,
+    video: "C6pFOTKbbCw?si=-9NEilnAmdBmU2X-",
+    interviewText: (
+      <>
+        <p>
+          Для&nbsp;Виталия спорт&nbsp;&mdash; это и&nbsp;хобби, и&nbsp;работа.
+          Его&nbsp;волонтерство началось с&nbsp;разовых соревнований
+          и&nbsp;мастер-классов для&nbsp;детей из&nbsp;отдаленных ямальских
+          поселков, позже переросло в&nbsp;большой социальный проект
+          &laquo;Все&nbsp;на&nbsp;матч&raquo;.
+        </p>
+        <p>
+          Зачем Виталий занимается с&nbsp;детьми, как&nbsp;совмещает работу
+          на&nbsp;Новопортовском промысле и&nbsp;тренировки, а&nbsp;также
+          чья&nbsp;поддержка для&nbsp;него особенно важна, узнаете
+          из&nbsp;интервью.
+        </p>
+      </>
+    ),
+    photos: [
+      {
+        id: 0,
+        big: vitaliiLezinPhoto1Big,
+        small: vitaliiLezinPhoto1Small,
+      },
+      {
+        id: 1,
+        big: vitaliiLezinPhoto2Big,
+        small: vitaliiLezinPhoto2Small,
+      },
+      {
+        id: 2,
+        big: vitaliiLezinPhoto3Big,
+        small: vitaliiLezinPhoto3Small,
+      },
+      {
+        id: 3,
+        big: vitaliiLezinPhoto4Big,
+        small: vitaliiLezinPhoto4Small,
+      },
+    ],
+  },
 ];
 
 export default heroes;
